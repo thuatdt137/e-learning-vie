62d3a3202c88bdc8b9e1f755448012d2b4ab9d16
quachthekiet <quachthekiet23122003@gmail.com>
2025-07-21 12:41:22 +0700

    update

---
+++ e-learning-vie/Controllers/AcademicYearManagement/AcademicYearsController.cs
@@ -0,0 +1,35 @@
+﻿using e_learning_vie.Commons;
+using e_learning_vie.DTOs.AcademicYear;
+using e_learning_vie.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_learning_vie.Controllers.AcademicYearManagement
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AcademicYearsController : ControllerBase
+    {
+
+        private readonly SchoolManagementContext _context;
+
+        public AcademicYearsController(SchoolManagementContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(ApiResponse<object>.Success("Success", _context.AcademicYears.Select(s => new AcademicYearDTO
+            {
+                AcademicYearId = s.AcademicYearId,
+                StartDate = s.StartDate,
+                EndDate = s.EndDate,
+                AspirationEditDeadline = s.AspirationEditDeadline,
+                AspirationRegistrationEndDate = s.AspirationRegistrationEndDate,
+                AspirationRegistrationStartDate = s.AspirationRegistrationStartDate,
+                YearName = s.YearName
+            })));
+        }
+
+    }
+}
---
+++ e-learning-vie/DTOs/AcademicYear/AcademicYearDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace e_learning_vie.DTOs.AcademicYear
+{
+    public class AcademicYearDTO
+    {
+        public int AcademicYearId { get; set; }
+
+        public string YearName { get; set; } = null!;
+
+        public DateOnly? StartDate { get; set; }
+
+        public DateOnly? EndDate { get; set; }
+
+        // Thời gian mở đăng ký nguyện vọng
+        public DateOnly? AspirationRegistrationStartDate { get; set; }
+
+        // Thời gian đóng đăng ký nguyện vọng
+        public DateOnly? AspirationRegistrationEndDate { get; set; }
+
+        // Thời gian đóng chỉnh sửa nguyện vọng (sau này chỉ xem được)
+        public DateOnly? AspirationEditDeadline { get; set; }
+    }
+}
