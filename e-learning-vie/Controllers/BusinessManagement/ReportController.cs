ba6eca5580026e4eb17d5f06847f668135d71433
thuatdt137 <dothuat098@gmail.com>
2025-07-25 10:58:22 +0700

    Create ReportController.cs

---
+++ e-learning-vie/Controllers/BusinessManagement/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using e_learning_vie.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_learning_vie.Controllers.BusinessManagement
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly SchoolManagementContext _context;
+        public ReportController(SchoolManagementContext context)
+        {
+            _context = context;
+        }
+    }
+}
