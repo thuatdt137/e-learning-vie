75f65306fb6ca2b4e086439d307deb8b6e209dd0
quachthekiet <quachthekiet23122003@gmail.com>
2025-07-25 12:02:43 +0700

    config academic rule

---
+++ e-learning-vie/DTOs/AcademicLevel/AcademicLevelRulesConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace e_learning_vie.DTOs.AcademicLevel
+{
+    public class AcademicLevelRulesConfig
+    {
+        public AcademicLevelRule Excellent { get; set; }
+        public AcademicLevelRule Good { get; set; }
+        public AcademicLevelRule Average { get; set; }
+        public AcademicLevelRule Weak { get; set; }
+    }
+}
