20a6726464c40a59df1995b7ac8f691b28874087
thuatdt137 <dothuat098@gmail.com>
2025-07-25 11:22:12 +0700

    models

--- e-learning-vie/Models/SubjectGroup.cs
+++ e-learning-vie/Models/SubjectGroup.cs
@@ -6,5 +6,8 @@
         public string SubjectGroupName { get; set; }
         public int LeadTeacherId { get; set; }
         public virtual Teacher LeadTeacher { get; set; }
+
+        public virtual ICollection<Subject> Subjects { get; set; } = new List<Subject>();
+
     }
 }
