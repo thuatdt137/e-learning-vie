5e7c75269e4679bcf771e5b830e5552094cff665
quachthekiet <quachthekiet23122003@gmail.com>
2025-07-26 09:38:51 +0700

    update

--- e-learning-vie/Controllers/BusinessManagement/PerformancesController.cs
+++ e-learning-vie/Controllers/BusinessManagement/PerformancesController.cs
@@ -9,33 +9,55 @@ namespace e_learning_vie.Controllers.BusinessManagement
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class PerformancesController : ControllerBase
     {
         private readonly SchoolManagementContext _context;
         private readonly AcademicLevelRulesConfig _rules;
         private readonly IStudentAcademicService _service;
+        private readonly IUserContextService _userContextService;
 
         private readonly ITrendAnalysisService _trendAnalysisService;
-        public PerformancesController(SchoolManagementContext context, IOptions<AcademicLevelRulesConfig> rules, IStudentAcademicService service, ITrendAnalysisService trendAnalysisService)
+        public PerformancesController(
+            SchoolManagementContext context,
+            IOptions<AcademicLevelRulesConfig> rules,
+            IStudentAcademicService service,
+            ITrendAnalysisService trendAnalysisService,
+            IUserContextService userContextService)
         {
             _context = context;
             _rules = rules.Value;
             _service = service;
             _trendAnalysisService = trendAnalysisService;
+            _userContextService = userContextService;
         }
 
+        //[Authorize(Roles = "Student, Teacher, Parent")]
         [HttpGet("scores/student")]
         public IActionResult GetStudentScores(int studentId, int semesterId)
         {
             try
             {
+                var userId = _userContextService.GetCurrentUserIdAsync().Result;
+
+                var user = _context.Users.Find(userId);
+
+                //if(user.StudentId != studentId)
+                //{
+                //    return StatusCode(StatusCodes.Status403Forbidden, ApiResponse<String>.Fail("Bạn không có quyền xem thông tin này"));
+                //}
+
+
+
+
                 return Ok(_service.GetStudentScores(studentId, semesterId));
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
-                return BadRequest(new { message = ex.Message });
+                return BadRequest(ApiResponse<String>.Error(ex.Message));
             }
         }
+        //[Authorize(Roles = "Teacher, HeaderDepartment")]
         [HttpGet("scores/class")]
         public IActionResult GetClassScores(int classId, int semesterId)
         {
@@ -43,12 +65,12 @@ namespace e_learning_vie.Controllers.BusinessManagement
             {
                 return Ok(_service.GetClassScores(classId, semesterId));
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
-                return BadRequest(new { message = ex.Message });
+                return BadRequest(ApiResponse<String>.Error(ex.Message));
             }
         }
-
+        //[Authorize(Roles = "Teacher, HeaderDepartment")]
         [HttpGet("academic-level/class")]
         public IActionResult GetClassAcademicLevel(int classId, int semesterId)
         {
@@ -56,11 +78,12 @@ namespace e_learning_vie.Controllers.BusinessManagement
             {
                 return Ok(_service.GetClassAcademicLevel(classId, semesterId));
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
-                return BadRequest(new { message = ex.Message });
+                return BadRequest(ApiResponse<String>.Error(ex.Message));
             }
         }
+        //[Authorize(Roles = "Teacher, HeaderDepartment, TrainingDepartment")]
         [HttpGet("academic-level/grade")]
         public IActionResult GetGradeAcademicLevel(int gradeId, int semesterId)
         {
@@ -70,7 +93,7 @@ namespace e_learning_vie.Controllers.BusinessManagement
             }
             catch(Exception ex)
             {
-                return BadRequest(new { message = ex.Message });
+                return BadRequest(ApiResponse<String>.Error(ex.Message));
             }
         }
         [HttpGet("grade-performance-trend/{gradeId}")]
